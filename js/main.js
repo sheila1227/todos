0c0f777ca8f1731346f02241d29c1ed3fca4ac24
Sheila1227 <sunjing1990123@126.com>
2015-04-16 11:38:28 +0800

    添加功能

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var todoItemCollection;
 window.onload = function() {
 	todoItemCollection = new TodoItemCollection("notes");
-	todoItemCollection.refreshItemsSummaryFunction=showActiveItemsCount;
+	todoItemCollection.refreshItemsSummaryFunction = RefreshItemsSummary;
+	todoItemCollection.DeleteItemsFunction=DeleteItems;
 	todoItemCollection.addItem("ttttttttttttsssss", TodoItem.COMPLETED);
 	todoItemCollection.addItem("HHHHHHH", TodoItem.ACTIVE);
 	todoItemCollection.addItem("sssssssssssss", TodoItem.COMPLETED);
@@ -30,30 +31,66 @@ window.onload = function() {
 		})(i);
 	}
 
+
+	//为“clear completed”按钮绑定事件
+	document.getElementById("btnClearCompletedItems").onclick=function(){
+		todoItemCollection.deleteItemsByState(TodoItem.COMPLETED);
+		RefreshItemsSummary();
+	}
+
 	//默认显示all items
 	document.getElementsByClassName("radio")[0].click();
 
 	//显示active items count
-	showActiveItemsCount();
+	RefreshItemsSummary();
 }
 
-function showActiveItemsCount(){
-	document.getElementById("activeItemsCount").getElementsByTagName("span")[0].innerText=todoItemCollection.getItemsCountByState(TodoItem.ACTIVE);
+function RefreshItemsSummary() {
+	showActiveItemsCount();
+	showCompletedItemsCount();
+}
+
+function DeleteItems(){
+	todoItemCollection.deleteItems();
+	RefreshItemsSummary();
+}
+
+function showActiveItemsCount() {
+	document.getElementById("activeItemsCount").getElementsByTagName("span")[0].innerText = todoItemCollection.getItemsCountByState(TodoItem.ACTIVE);
+}
+
+function showCompletedItemsCount() {
+	var btnClearCompletedItems = document.getElementById("btnClearCompletedItems");
+	var count = todoItemCollection.getItemsCountByState(TodoItem.COMPLETED);
+	btnClearCompletedItems.getElementsByTagName("span")[0].innerText = count;
+	if (count < 1) {
+		btnClearCompletedItems.setAttribute("style", "display:none;");
+	} else {
+		btnClearCompletedItems.setAttribute("style", "display:inline;");
+	}
 }
 
 function TodoItemCollection(ulID) {
 	this.todoItems = new Array();
-	this.itemAppearanceSetting = new ItemAppearanceSetting("✔", "toggle-state-widget-before-selected", "toggle-state-widget-selected");
+	this.itemAppearanceSetting = new ItemAppearanceSetting("✔", "×", "toggle-state-widget-before-selected", "toggle-state-widget-selected");
 	this.ulNode = document.getElementById(ulID);
 }
 
 TodoItemCollection.prototype.addItem = function(text, state) {
-	var toDoItem = new TodoItem(text, state, this.itemAppearanceSetting,this.refreshItemsSummaryFunction);
+	var toDoItem = new TodoItem(text, state, this.itemAppearanceSetting, this.refreshItemsSummaryFunction,this.DeleteItemsFunction);
 	this.todoItems.push(toDoItem);
 	this.ulNode.appendChild(toDoItem.liNode);
 }
 
-TodoItemCollection.prototype.removeItem = function() {
+TodoItemCollection.prototype.deleteItemsByState = function(state) {
+	for (var i = 0; i < this.todoItems.length; i++) {
+		var item = this.todoItems[i];
+		if (item.state === state) {
+			this.ulNode.removeChild(item.liNode);
+			this.todoItems.splice(i,1);
+			i--;
+		}
+	}
 
 }
 
@@ -102,14 +139,17 @@ TodoItemCollection.prototype.showAllItems = function() {
 
 }
 
-function TodoItem(text, state, itemAppearanceSetting,refreshItemsSummaryFunction) {
+function TodoItem(text, state, itemAppearanceSetting, refreshItemsSummaryFunction, deleteItemsFunction) {
 	this.text = text;
 	this.state = state;
 	this.itemAppearanceSetting = itemAppearanceSetting;
 	this.liNode = getTodoItemLiNode(this.text, this.itemAppearanceSetting);
-	this.refreshItemsSummaryFunction=refreshItemsSummaryFunction;
+	//this.refreshItemsSummaryFunction = refreshItemsSummaryFunction;
+	//this.deleteItemsFunction=deleteItemsFunction;
 	var _that = this;
 	var _lbToggle = this.liNode.getElementsByTagName("label")[0];
+	var _btnDestroy = this.liNode.getElementsByTagName("button")[0];
+	/*给每行前面的状态切换标签绑定事件*/
 	(function(that, lbToggle) {
 		lbToggle.onclick = function() {
 			if (that.state === TodoItem.ACTIVE) {
@@ -118,12 +158,21 @@ function TodoItem(text, state, itemAppearanceSetting,refreshItemsSummaryFunction
 				that.activate();
 			}
 
-			if(that.refreshItemsSummaryFunction!=null){
-				that.refreshItemsSummaryFunction();
+			if (refreshItemsSummaryFunction != null) {
+				refreshItemsSummaryFunction();
 			}
 		}
 	})(_that, _lbToggle);
-
+	/*给每行后面的删除按钮绑定事件*/
+	(function(that, btnDestroy) {
+		btnDestroy.onclick = function() {
+			that.delete();
+			if (deleteItemsFunction != null) {
+				deleteItemsFunction(TodoItem.DELETED);
+			}
+		}
+	})(_that, _btnDestroy);
+	/*根据传入的参数初始化状态*/
 	(function(that) {
 		if (that.state === TodoItem.ACTIVE) {
 			that.activate();;
@@ -143,8 +192,11 @@ function getTodoItemLiNode(text, itemAppearanceSetting) {
 	lbToggle.innerText = itemAppearanceSetting.toggleAppearance;
 	var lbTodoContent = document.createElement("label");
 	lbTodoContent.innerText = text;
+	var btnDestroy = document.createElement("button");
+	btnDestroy.innerText = itemAppearanceSetting.destroyAppearance;
 	li.appendChild(lbToggle);
 	li.appendChild(lbTodoContent);
+	li.appendChild(btnDestroy);
 	return li;
 }
 
@@ -159,7 +211,7 @@ TodoItem.prototype.complete = function() {
 }
 
 TodoItem.prototype.delete = function() {
-
+	this.state = TodoItem.DELETED;
 }
 
 TodoItem.prototype.setVisibility = function(visible) {
@@ -171,8 +223,9 @@ TodoItem.prototype.setVisibility = function(visible) {
 }
 
 
-function ItemAppearanceSetting(toggleAppearance, beforeSelectedClassName, selectedClassName) {
+function ItemAppearanceSetting(toggleAppearance, destroyAppearance, beforeSelectedClassName, selectedClassName) {
 	this.toggleAppearance = toggleAppearance;
+	this.destroyAppearance = destroyAppearance;
 	this.beforeSelectedClassName = beforeSelectedClassName;
 	this.selectedClassName = selectedClassName;
 }
